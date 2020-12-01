91a37dd01131f55a5cab6b96bd24995b8a0bf740
David Frankland <david@dmf-software.co.uk>
2020-12-01 15:20:30 +0000

    Completed day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var inputValues = File.ReadLines("input.txt").Select(x => Convert.ToInt32(x)).ToList();
+
+            Console.WriteLine(Part1(inputValues));
+            Console.WriteLine(Part2(inputValues));
+        }
+
+        private static int Part1(List<int> inputValues)
+        {
+            foreach (var inputValue1 in inputValues)
+            {
+                foreach (var inputValue2 in inputValues)
+                {
+                    if (inputValue1 + inputValue2 == 2020)
+                    {
+                        return inputValue1 * inputValue2;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        private static int Part2(List<int> inputValues)
+        {
+            foreach (var inputValue1 in inputValues)
+            {
+                foreach (var inputValue2 in inputValues)
+                {
+                    foreach (var inputValue3 in inputValues)
+                    {
+                        if (inputValue1 + inputValue2 + inputValue3 == 2020)
+                        {
+                            return inputValue1 * inputValue2 * inputValue3;
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
