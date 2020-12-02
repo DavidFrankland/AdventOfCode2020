795e27fdd911c63942cf3e61e35c539b7484d4a0
David Frankland <david@dmf-software.co.uk>
2020-12-02 19:42:39 +0000

    created skeleton project

---
+++ Skeleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Skeleton
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // integers
+            //var inputValues = File.ReadLines("input.txt").Select(x => Convert.ToInt32(x)).ToList();
+
+            // strings
+            var inputValues = File.ReadLines("input.txt").ToList();
+
+            Console.WriteLine($"There are {inputValues.Count} values");
+        }
+    }
+}
